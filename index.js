539af246d00fe28c8d5ef55e42cd47e673d66767
Ryan Werner <rmwerner@umich.edu>
2020-05-19 01:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInAnArray(array, callback) {
-  array.forEach(callback)
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
